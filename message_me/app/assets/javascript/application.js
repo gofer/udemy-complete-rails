6a0896902c1a70b4a6012aeb6419bc1fc77f3b05
gofer@ex-studio.info <Gofer>
2025-08-12 11:15:02 +0000

    236 Add auto-scrolling to chat window

--- message_me/app/assets/javascript/application.js
+++ message_me/app/assets/javascript/application.js
@@ -1,11 +1,19 @@
 // Loads all Semantic javascripts
 //= require semantic-ui
 
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 // ドロップダウンを使用するためのコード
-$(document).on('turbolinks:load', function () {
+$(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
 
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+
+  scroll_bottom();
 });
