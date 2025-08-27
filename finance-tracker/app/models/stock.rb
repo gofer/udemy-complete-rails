b097e471ef84fb852e38ae8a0ac94d867d5257ae
gofer@ex-studio.info <Gofer>
2025-08-27 15:55:43 +0000

    263 Dealing with invalid search results

--- finance-tracker/app/models/stock.rb
+++ finance-tracker/app/models/stock.rb
@@ -2,9 +2,16 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     # client = IEX::Api::Client.new(publishable_token: '<<secret_token>>',
     #                               endpoint: 'https://sandbox.iexapis.com/v1')
-    # client.price(ticker_symbol)
+    # begin
+    #   client.price(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    # rescue => exception
+    # end
 
     client = FmpClient.new(Rails.application.credentials.fmp_client[:api_key], true)
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
 end
